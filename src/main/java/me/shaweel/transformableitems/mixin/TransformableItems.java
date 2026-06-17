26475b40595263fc591e807c2a317618d78a7190
shaweel <shaweel@proton.me>
2026-06-17 16:03:58 +0200

    refactor: remove useless import

--- src/main/java/me/shaweel/transformableitems/mixin/TransformableItems.java
+++ src/main/java/me/shaweel/transformableitems/mixin/TransformableItems.java
@@ -12,7 +12,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import static com.mojang.blaze3d.platform.GlStateManager.popMatrix;
 import static com.mojang.blaze3d.platform.GlStateManager.pushMatrix;
 import static com.mojang.blaze3d.platform.GlStateManager.translated;
 import static com.mojang.blaze3d.platform.GlStateManager.scalef;
