37ebfbe7afd7636b3a91ac1f95e18fb36b8f1283
shaweel <shaweel@proton.me>
2026-06-18 21:09:35 +0200

    feat: start basic forge project

---
+++ src/main/java/me/shaweel/transformableitems/TransformableItems.java
@@ -0,0 +1,11 @@
+package me.shaweel.transformableitems;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod("transformableitems")
+public class TransformableItems {
+	public TransformableItems(FMLJavaModLoadingContext context) {
+		System.out.println("Hello Forge World!");
+	}
+}
