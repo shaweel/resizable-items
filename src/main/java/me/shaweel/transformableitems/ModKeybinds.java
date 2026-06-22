6fc31bf98b7abefeeadec38344c6cc471c9deea0
shaweel <shaweel@proton.me>
2026-06-22 19:24:43 +0200

    chore: 1.21.9-1.21.10-forge

--- src/main/java/me/shaweel/transformableitems/ModKeybinds.java
+++ src/main/java/me/shaweel/transformableitems/ModKeybinds.java
@@ -2,13 +2,13 @@ package me.shaweel.transformableitems;
 
 import org.lwjgl.glfw.GLFW;
 import net.minecraft.client.KeyMapping;
-import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceLocation;
 
 public class ModKeybinds {
 	public static KeyMapping OPEN_CONFIG_KEYBIND;
 
 	public static void initialize() {
-		KeyMapping.Category category = KeyMapping.Category.register(Identifier.fromNamespaceAndPath("transformableitems", "transformableitems"));
+		KeyMapping.Category category = KeyMapping.Category.register(ResourceLocation.fromNamespaceAndPath("transformableitems", "transformableitems"));
 		OPEN_CONFIG_KEYBIND = new KeyMapping("key.transformableitems.open_config", GLFW.GLFW_KEY_F7, category);
 	}
 }
