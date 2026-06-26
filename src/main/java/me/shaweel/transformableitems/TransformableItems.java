a9678397f41c31d73f022e0e3262806847c5356d
shaweel <shaweel@proton.me>
2026-06-26 18:42:48 +0200

    fix it

--- src/main/java/me/shaweel/transformableitems/TransformableItems.java
+++ src/main/java/me/shaweel/transformableitems/TransformableItems.java
@@ -1,5 +1,9 @@
 package me.shaweel.transformableitems;
 
+import org.spongepowered.asm.launch.MixinBootstrap;
+import org.spongepowered.asm.mixin.MixinEnvironment;
+import org.spongepowered.asm.mixin.Mixins;
+
 import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
@@ -9,5 +13,8 @@ public class TransformableItems {
 	public TransformableItems() {
 		ConfigFile.load();
 		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.CONFIGGUIFACTORY, () -> (client, parent) -> new ConfigScreen());
+		MixinBootstrap.init();
+		Mixins.addConfiguration("transformableitems.mixins.json");
+		MixinEnvironment.getDefaultEnvironment().setSide(MixinEnvironment.Side.CLIENT);
 	}
 }
