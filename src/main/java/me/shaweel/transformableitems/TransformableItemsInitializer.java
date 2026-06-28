e7678a51c92a6bc2952a884d48323d309fdf1789
shaweel <shaweel@proton.me>
2026-06-29 00:40:48 +0200

    fix: enable config button

--- src/main/java/me/shaweel/transformableitems/TransformableItemsInitializer.java
+++ src/main/java/me/shaweel/transformableitems/TransformableItemsInitializer.java
@@ -1,14 +1,49 @@
 package me.shaweel.transformableitems;
 
+import java.lang.reflect.Field;
+import java.util.List;
+
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.loading.moddiscovery.ModInfo;
 
 @Mod("transformableitems")
 public class TransformableItemsInitializer {
+	private static class PatchedModInfo extends ModInfo {
+		public PatchedModInfo(ModInfo modInfo) {
+			super(modInfo.getOwningFile(), modInfo.getModConfig());
+		}
+		
+		@Override
+		public boolean hasConfigUI() {
+			return true;
+		}
+	}
+
 	public TransformableItemsInitializer() {
 		ConfigFile.load();
+		
+		try {
+			ModList modList = ModList.get();
+
+			Field sortedListField = ModList.class.getDeclaredField("sortedList");
+			sortedListField.setAccessible(true);
+
+			@SuppressWarnings("unchecked")
+			List<ModInfo> sortedList = (List<ModInfo>) sortedListField.get(modList);
+
+			ModInfo modInfo = sortedList.stream().filter(info -> info.getModId().equals("transformableitems")).findFirst().get();
+
+			sortedList.set(sortedList.indexOf(modInfo), new PatchedModInfo(modInfo));
+			sortedListField.set(modList, sortedList);
+		} catch (Exception e) {
+			System.err.println("Failed to load config:");
+			e.printStackTrace();
+		}
+
 		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.CONFIGGUIFACTORY, () -> (client, parent) -> new ConfigScreen());
 		MinecraftForge.EVENT_BUS.register(ModKeybinds.class);
 		MinecraftForge.EVENT_BUS.register(ItemHeightAnimations.class);
